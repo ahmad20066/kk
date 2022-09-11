9f05416ff8a4f456ee45a81f6df7f31ad21aa235
ahmad afif <ahmadafif613@gmail.com>
2022-09-11 22:53:40 +0300

    3

--- routes/categroy.js
+++ routes/categroy.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/category_controller');
 router.get('/',controller.getCategories);
-router.post('/',controller.createCategory);
+router.post('/',controller.addCategory);
 router.get('/:section',controller.getCategoriesBySection);//get categories by section
 module.exports = router;
