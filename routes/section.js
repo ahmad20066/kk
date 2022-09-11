1d2602f8d54934692ae0ea78f96f510c6c6bbf90
ahmad afif <ahmadafif613@gmail.com>
2022-09-11 22:50:12 +0300

    3

--- routes/section.js
+++ routes/section.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const controller = require('../controllers/section_controller');
 router.post('/',controller.addSection);
 router.get('/',controller.getSections);
+router.get('/:sectionId',controller.getSectionById);
 module.exports = router;
